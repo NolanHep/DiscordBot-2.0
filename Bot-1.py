74c444a942793a5547b11d36cf3c44b7a4c5deb6
NolanHep <nolan.hepworth@greenwoodcollege.com>
2018-10-30 10:02:53 -0400

    Update - TOKEN

--- Bot-1.py
+++ Bot-1.py
@@ -1,13 +1,19 @@
 import discord
 from discord.ext import commands
 
-TOKEN = ''
+TOKEN = 'NTA2ODIzNzQyODc4Mzg0MTQ4.DrnwJw.lPqpVJWqk0iqz0XfscA9_p8WZW0'
 
 client = commands.Bot(command_prefix = '!')
 
 @client.event
 async def on_ready():
     print('Bot Ready.')
+
+@client.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    print('{}: {}'.format(author, content))
 
 client.run(TOKEN)
 
